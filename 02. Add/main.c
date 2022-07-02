ca05fdfbad532661b6d0cd3485dcdeb37a72f75a
Mahak Kaur Chhabra <95758357+mahak1808@users.noreply.github.com>
2022-07-03 00:27:30 +0530

    Add files via upload

---
+++ 02. Add/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Intro
+//
+//  Created by Mahak Kaur Chhabra on 25/10/21.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    a=10, b=7, c=a+b;
+    printf ("The sum of a and b is %d.",c);
+    return 0;
+    
+}
---
+++ 03. Cube of a number/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Cube of a number
+//
+//  Created by Mahak Kaur Chhabra on 26/10/21.
+//
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("The cube of number is %d.",num*num*num);
+    return 0;
+}
